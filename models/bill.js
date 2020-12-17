a1f0779074f325f7401079afacc365ea70e39533
Rocco <roccomaniscalco2001@gmail.com>
2020-12-17 15:53:21 -0500

    added title and description columns to bill

--- models/bill.js
+++ models/bill.js
@@ -5,6 +5,14 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       allowNull: false,
     },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     yeas: {
       type: DataTypes.INTEGER,
       defaultValue: 0,
