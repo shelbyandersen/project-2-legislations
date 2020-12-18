b1a1bd66ec76761b60c4a789eb5b5427185f1493
Shelby <shelbyrobison14@gmail.com>
2020-12-17 23:48:45 -0500

    added axios to generate api call to legislation page. Styling is [WIP]

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const axios = require("axios");
+const API_KEY = "1746e141194bb3c5a7187530792f8912";
+const STATE = "GA";
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
 router.get("/legislation", (req, res) => {
-  res.render("legislation");
+  axios({
+    method: 'get',
+    url: `https://api.legiscan.com/?key=${API_KEY}&op=search&state=${STATE}`,
+  })
+    .then(function (response) {
+  res.render("legislation", {
+    bills: Object.values(response.data.searchresult)
+  });
+    });
 });
 
 router.get("/account", (req, res) => {
